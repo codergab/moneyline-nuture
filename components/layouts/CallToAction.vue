a4e9e1fa94c79ef672140cc27c7751fa7f3586c0
christian <christian.onyeneke@arkounting.com.ng>
2019-05-04 15:27:20 +0100

    Created CallToAction component

---
+++ components/layouts/CallToAction.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="applynow text-center">
+        Get a loan of up to <span class="orange">N10,000,000</span> with MoneyLine within 48 hours
+        <br>
+        <a href="#" class="terms"><span>*Terms and conditions apply</span></a>
+        <br>
+        <a href="#" class="btn btn-primary mt-2">APPLY NOW</a>
+    </div>
+</template>
+
+<script>
+export default {
+	name: "CallToAction"
+};
+</script>
+
+
+<style scoped>
+    .orange {
+        color: orange;
+    }
+    .terms {
+        font-size: 9px;
+        color: #fff;
+    }
+    .terms:hover {
+        text-decoration: underline;
+    }
+    .applynow {
+        background: #013a59;
+        color: #fff;
+        padding: 30px;
+    }
+</style>
--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,29 +1,35 @@
 <template>
 	<div>
 		<Header/>
-		<div class="slidee padded">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="payoff">
-                            We listen to your <br>
-                            <span class="highlight">financial</span> needs when no<br>
-                            one else does
+		<div class="row">
+            <div class="col-md-12">
+                <div class="slidee">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="payoff">
+                                We listen to your <br>
+                                <span class="highlight">financial</span> needs when no<br>
+                                one else does
+                            </div>
                         </div>
                     </div>
                 </div>
+                <CallToAction/>
             </div>
-            <img src="/Artboard – 3.png" style="width: 100%;  background-size: cover;">
-		</div>
+        </div>
 	</div>
 </template>
 <style scoped>
+    .slidee {
+        background-image: url('/Artboard–3.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
     .payoff {
         font-size: 2.15em;
         font-weight: 200;
-        position: absolute;
         color: #fff;
-        padding: 111px 0 0 0px;
+        padding: 111px 0 145px 30px;
     }
     .highlight {
         font-weight: bold;
@@ -31,9 +37,11 @@
 </style>
 <script>
 import Header from "@/components/layouts/Header";
+import CallToAction from "@/components/layouts/CallToAction"
 export default {
 	components: {
-		Header
+        Header,
+        CallToAction
 	}
 };
 </script>
