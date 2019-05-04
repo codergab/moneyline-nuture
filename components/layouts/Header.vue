415b8a7d948d41ef2c30f340b38f2967324662a7
Gabriel Adewumi <codergab@gmail.com>
2019-05-04 12:41:33 +0100

    Working on Homepage

---
+++ components/layouts/Header.vue
@@ -0,0 +1,58 @@
+<template>
+	<b-navbar toggleable="lg" type="light" variant="light">
+		<div class="container">
+			<b-navbar-brand href="#">
+				<img src="/logo.png" width="200" alt="Kitten">
+			</b-navbar-brand>
+			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+			<b-collapse id="nav-collapse" is-nav>
+				<!-- Right aligned nav items -->
+				<b-navbar-nav class="ml-auto">
+					<b-navbar-nav>
+						<b-nav-item href="#">Home</b-nav-item>
+						<b-nav-item href="#">About Us</b-nav-item>
+						<b-nav-item href="#">FAQ</b-nav-item>
+						<b-nav-item href="#">Contact</b-nav-item>
+						<b-button class="my-2 my-sm-0" variant="primary" size="sm">Register</b-button>&nbsp;
+						<b-button class="my-2 my-sm-0" variant size="sm">Login</b-button>
+					</b-navbar-nav>
+
+					<!-- <b-nav-item-dropdown text="Lang" right>
+						<b-dropdown-item href="#">EN</b-dropdown-item>
+						<b-dropdown-item href="#">ES</b-dropdown-item>
+						<b-dropdown-item href="#">RU</b-dropdown-item>
+						<b-dropdown-item href="#">FA</b-dropdown-item>
+					</b-nav-item-dropdown>
+
+					<b-nav-item-dropdown right>
+					Using 'button-content' slot-->
+					<!-- <template slot="button-content">
+							<em>User</em>
+						</template>
+						<b-dropdown-item href="#">Profile</b-dropdown-item>
+						<b-dropdown-item href="#">Sign Out</b-dropdown-item>
+					</b-nav-item-dropdown>-->
+				</b-navbar-nav>
+			</b-collapse>
+		</div>
+	</b-navbar>
+</template>
+
+<script>
+export default {
+	name: "Header"
+};
+</script>
+
+
+<style scoped>
+.navbar {
+	padding: 1rem 1rem;
+}
+
+.bg-light {
+	background-color: #ffffff !important;
+	box-shadow: 0px 1px 9px rgba(193, 193, 193, 0.1);
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,40 +4,55 @@ export default {
   mode: 'spa',
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+    title: 'Moneyline',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: pkg.description
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/icon.png'
+    }, {
+      rel: 'stylesheet',
+      'href': 'https://fonts.googleapis.com/css?family=Lato|Roboto:400,700'
+    }]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
 
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
+    '~/assets/css/main.css'
   ],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
@@ -45,20 +60,19 @@ export default {
     'bootstrap-vue/nuxt',
   ],
   /*
-  ** Axios module configuration
-  */
+   ** Axios module configuration
+   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
