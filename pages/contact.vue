35481a5dc1b2dcac57b9d798e993bfb60dabcff9
devchuksemeka <m.okafor@nuture.tech>
2019-05-04 14:42:34 +0100

    added background image to jumbotron

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,8 +2,8 @@
 	<div>
 		<Header/>
 		<div>
-			<b-jumbotron>
-				
+			<b-jumbotron class="bg_artboard_1" style="height:400px">
+					<p class="text-white jumbotron_content">We provide <span class="bold">smart loans</span><br>designed to put you at <br>ease</p>
 			</b-jumbotron>
 		</div>
 	</div>
@@ -22,3 +22,18 @@ export default {
 	}
 };
 </script>
+
+<style scoped>
+	.bg_artboard_1{
+		background-image:  url('/Artboard – 1.png');
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+	.bold{
+		font-weight: 600
+	}
+	.jumbotron_content{
+		font-size: 22px
+	}
+</style>
+
