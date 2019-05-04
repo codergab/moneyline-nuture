9abf3021f5f35aea6f3300e807bfe098bdbfea5d
christian <christian.onyeneke@arkounting.com.ng>
2019-05-04 14:40:43 +0100

    started aboutus

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -10,10 +10,10 @@
 				<!-- Right aligned nav items -->
 				<b-navbar-nav class="ml-auto">
 					<b-navbar-nav>
-						<b-nav-item href="#">Home</b-nav-item>
-						<b-nav-item href="#">About Us</b-nav-item>
-						<b-nav-item href="#">FAQ</b-nav-item>
-						<b-nav-item href="#">Contact</b-nav-item>
+						<b-nav-item to="/">Home</b-nav-item>
+						<b-nav-item to="/about-us">About Us</b-nav-item>
+						<b-nav-item to="/faq">FAQ</b-nav-item>
+						<b-nav-item to="/contact">Contact</b-nav-item>
 						<b-button class="my-2 my-sm-0" variant="primary" size="sm">Register</b-button>&nbsp;
 						<b-button class="my-2 my-sm-0" variant size="sm">Login</b-button>
 					</b-navbar-nav>
---
+++ pages/about-us.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<Header/>
+		<div class="slidee padded">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="payoff">
+                            We listen to your <br>
+                            <span class="highlight">financial</span> needs when no<br>
+                            one else does
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img src="/Artboard – 3.png" style="width: 100%;  background-size: cover;">
+		</div>
+	</div>
+</template>
+<style scoped>
+    .payoff {
+        font-size: 2.15em;
+        font-weight: 200;
+        position: absolute;
+        color: #fff;
+        padding: 111px 0 0 0px;
+    }
+    .highlight {
+        font-weight: bold;
+    }
+</style>
+<script>
+import Header from "@/components/layouts/Header";
+export default {
+	components: {
+		Header
+	}
+};
+</script>
