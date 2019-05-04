b8816ce8d6d0c14ded36c6df956f5e178bd7aaf7
devchuksemeka <m.okafor@nuture.tech>
2019-05-04 15:28:27 +0100

    update contact page

---
+++ components/layouts/CallToAction.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="bg-color">
+			<h1>Action Apply Now</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "CallToAction"
+};
+</script>
+
+
+<style scoped>
+.bg-color{
+	background-color: #025786 !important
+}
+
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -2,14 +2,69 @@
 	<div>
 		<Header/>
 		<div>
-			<b-jumbotron class="bg_artboard_1" style="height:400px">
+			<b-jumbotron class="bg_artboard_1" style="height:550px">
 					<p class="text-white jumbotron_content">We provide <span class="bold">smart loans</span><br>designed to put you at <br>ease</p>
 			</b-jumbotron>
 		</div>
+		<apply-now></apply-now>
+		<b-container class="bv-example-row">
+			<b-row>
+				<b-col>
+					<b-card>
+						<div>
+						<b-form @submit="onSubmit">
+							<b-form-group id="input-group-2" label="Name:" label-for="name">
+								<b-form-input id="name" required ></b-form-input>
+							</b-form-group>
+							<b-form-group
+								id="email-group"
+								label="Email:"
+								label-for="email"
+							>
+								<b-form-input
+								id="email"
+								type="email"
+								required
+								></b-form-input>
+							</b-form-group>
+							<b-form-group
+								id="company-description-group"
+								label="Company Description:"
+								label-for="company_description"
+							>
+								<b-form-textarea
+									id="company_description"
+									rows="3"
+									></b-form-textarea>
+
+							</b-form-group>
+							<b-form-group
+								id="message-group"
+								label="Message:"
+								label-for="message"
+							>
+								<b-form-textarea
+									id="message"
+									rows="6"
+									></b-form-textarea>
+
+							</b-form-group>
+						<b-button type="submit" variant="primary">SEND MESSAGE</b-button>
+						</b-form>
+					</div>
+					</b-card>
+				</b-col>
+				<b-col>
+					
+				</b-col>
+			</b-row>
+		</b-container>
+		<apply-now></apply-now>
 	</div>
 </template>
 <script>
 import Header from "@/components/layouts/Header";
+import CallToAction from "@/components/layouts/CallToAction";
 export default {
 	"name":"Contact",
 	data(){
@@ -18,7 +73,8 @@ export default {
 		}
 	},
 	components: {
-		Header
+		Header,
+		'apply-now' : CallToAction
 	}
 };
 </script>
