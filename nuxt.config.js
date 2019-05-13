38b87802c766bed47c446ea36478c132b1b6c928
Gabriel Adewumi <codergab@gmail.com>
2019-05-13 18:52:10 +0100

    Updated User Page

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,8 +87,8 @@ export default {
    */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: 'http://localhost:8080/api/v1'
-    // baseURL: 'https://api.moneyline.ng/api/v1'
+    // baseURL: 'http://localhost:8080/api/v1'
+    baseURL: 'https://api.moneyline.ng/api/v1'
   },
 
   /*
